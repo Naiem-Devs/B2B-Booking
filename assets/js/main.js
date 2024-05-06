f68efefc7cab222b494bb0f67ef5a835571ab57f
Naiem-Devs <dnsnaiem@gmail.com.com>
2024-05-06 22:43:11 +0600

    home-page

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+  
+ // menu 
+  $('.lng_btn').click( function (){ 
+    $('.lng__blk').toggleClass('show');   
+    $('.notifi__blk').removeClass('show');   
+  }); 
+  
+ // menu 
+  $('.notifi_btn').click( function (){ 
+    $('.notifi__blk').toggleClass('show');  
+    $('.lng__blk').removeClass('show');  
+  }); 
+  
+
+
+
+  $('.select__blk select').niceSelect();
+
+  // owlCarousel
+  $(".brand-active").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 6,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+      767: {
+        items: 3
+      },
+      992: {
+        items: 6
+      }
+    }
+  });
+
+
+ 
+})(jQuery);
