aea2999684cab44192b871589ce4dd97d127623f
Naiem-Devs <dnsnaiem@gmail.com.com>
2024-05-16 02:53:32 +0600

    page-added

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,9 +25,67 @@
   }); 
   
 
-
-
   $('.select__blk select').niceSelect();
+
+  // owlCarousel
+  $(".flight__slider").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 6,
+    center: true,
+    navText: [
+      '<i class="fal fa-chevron-circle-left"></i>',
+      '<i class="fal fa-chevron-circle-right"></i>'
+    ],
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+      767: {
+        items: 4
+      },
+      992: {
+        items: 5
+      },
+      1200: {
+        items: 6
+      },
+      1400: {
+        items: 7
+      }
+    }
+  });
+
+  // owlCarousel
+  $(".brands__flight__slider").owlCarousel({
+    loop: true,
+    margin: 20,
+    navText: [
+      '<i class="fal fa-chevron-circle-left"></i>',
+      '<i class="fal fa-chevron-circle-right"></i>'
+    ],
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      767: {
+        items: 2
+      },
+      992: {
+        items: 2
+      },
+      1200: {
+        items: 2
+      },
+      1400: {
+        items: 2
+      }
+    }
+  });
 
   // owlCarousel
   $(".brand-active").owlCarousel({
